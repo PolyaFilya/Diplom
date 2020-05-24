92166f2ef2ecf09b191bfccb89b0558e565f2646
PolyaFilya <pollyfilippova51@gmail.com>
2020-05-24 22:50:52 +0500

    version 4

--- registr/urls.py
+++ registr/urls.py
@@ -6,18 +6,16 @@ from . import views
 urlpatterns = [
     #ex: /registr/
     path('', views.index, name='index'),
-    #ex: /registr/1/choice
-    path('<int:event_id>/choice/', views.choice, name='choice'),
-    #ex: /registr/5
+    #ex: /registr/choice
+    path('choice/', views.choice, name='choice'),
+    #ex: /registr/5/new_user
     path('<int:event_id>/new_user', views.new_user, name='new_user'),
     #ex: /registr/5/done
     path('<int:user_id>/done/', views.reg_done, name='reg_done'),
     #wx: /registr/5/done/qr
     path('<int:user_id>/done/qr/', views.qr, name='qr'),
-    #ex: /registr/1/scan
-    path('<int:event_id>/scan/', views.scan, name='scan'),
-    #ex: /registr/1/scan
-    #path('<int:user_id>/scan_qr/', views.scan_qr, name='scan_qr'),
+    #ex: /registr/scan
+    path('scan/', views.scan, name='scan'),
     #ex: /registr/100/here
     path('<int:user_id>/here/', views.user_here, name='user_here'),
 ]
--- registr/views.py
+++ registr/views.py
@@ -10,30 +10,23 @@ from .models import User, Event, State, QR
 from .forms import QRForm
 
 def index(request):
+    return render(request, 'registr/index.html')
+
+def choice(request):
     event_list = Event.objects.all()
     context = { 'event_list': event_list }
-    return render(request, 'registr/index.html', context)
-
-def choice(request, event_id):
-    event = Event.objects.get(pk=event_id)
-    event_name = event.event_name
-    new_user_id = User.objects.latest('id').id + 1
-    context = { 
-        'event_name': event_name,
-        'user_id': new_user_id,
-        'event_id': event_id,
-    }
     return render(request, 'registr/choice.html', context)
 
 def new_user(request, event_id):
     new_user_id = User.objects.latest('id').id + 1
+    event = Event.objects.get(pk=event_id)
     #тут типа должно быть добавление данных в учетную запись
     try:
-        new_user = User.objects.create(first_name=request.POST['name'], gender=request.POST['gender'], email=request.POST['email'], age=request.POST['age'], event_id=request.POST['event_id'])
+        new_user = User.objects.create(first_name=request.POST['name'], gender=request.POST['gender'], email=request.POST['email'], age=request.POST['age'], event_id=event.id)
         reg_state = new_user.state_set.create(name_state="reg", user_id=new_user.id, reg_time=timezone.now())
     except (KeyError, User.DoesNotExist):
         return render(request, 'registr/new_user.html', {
-            'user_id': new_user_id,
+            'event': event,
             'error_message': "Вы не ввели данные",
         })
     else:
@@ -54,8 +47,7 @@ def qr(request, user_id):
     qr.save(response, "PNG")
     return response
 
-def scan(request, event_id):
-    event = Event.objects.get(pk=event_id)
+def scan(request):
     # тут еще типа надо вставить сканирование QR, получение id пользователя
     if request.method == 'POST':
         form = QRForm(request.POST, request.FILES)
@@ -65,18 +57,28 @@ def scan(request, event_id):
             qr.file = form.cleaned_data["file"]
             qr.file.save('qr.png', content=qr.file, save=True)
             user_id = scanner('registr/files/qrs/qr.png')
+            who = User.objects.get(pk=user_id)
+            s = who.state_set.count()
+            #if s == 1:
             user = User.objects.get(pk=user_id)
             user.state_set.create(name_state="here", user_id=user.id, reg_time=timezone.now())
             qr.file.delete()
+            qr.delete()
             return HttpResponseRedirect(reverse('user_here', args=(user_id, )))
+        else:
+            form = QRForm()
+                #error = 'Этот человек уже прошел'
+                #qr.file.delete()
+                #qr.delete()
+                #return render(request, 'registr/scan.html', {'form': form, 'error': error})
     else:
         form = QRForm()
 
-    return render(request, 'registr/scan.html', {'form': form, 'event': event })
+    return render(request, 'registr/scan.html', {'form': form, })
 
 def user_here(request, user_id):
     try:
-        first_name = User.objects.get(pk=user_id).first_name
+        first_name = User.objects.get(pk=user_id).first_name   
     except User.DoesNotExist:
         raise Http404("User does not exist")
     return render(request, 'registr/user_here.html', { 'first_name': first_name })
