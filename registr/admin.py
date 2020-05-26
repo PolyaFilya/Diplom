be8899ef34d2bda4c99edc192f0319ba3b07326f
PolyaFilya <pollyfilippova51@gmail.com>
2020-05-26 19:50:59 +0500

    version5.6

--- registr/admin.py
+++ registr/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Event
+from .models import Event, User, State
 
 admin.site.register(Event)
+admin.site.register(User)
+admin.site.register(State)
