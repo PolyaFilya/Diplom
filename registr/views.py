4b84f2a41eed6bbb595fcdb20d68e2f7e8ce29e6
PolyaFilya <pollyfilippova51@gmail.com>
2020-05-31 23:07:27 +0500

    version5.8

--- registr/views.py
+++ registr/views.py
@@ -54,31 +54,39 @@ def qr(request, user_id):
     return response
 
 def scan(request):
-    # тут еще типа надо вставить сканирование QR, получение id пользователя
+    # тут сканирование QR, получение id пользователя
     if request.method == 'POST':
-        form = QRForm(request.POST, request.FILES)
-        if form.is_valid():
-            #тут должна быть выгрузка файла из формы 
-            qr = QR()
-            qr.file = form.cleaned_data["file"]
-            qr.file.save('qr.png', content=qr.file, save=True)
-            user_id = scanner('registr/files/qrs/qr.png')
-            who = User.objects.get(pk=user_id)
-            s = who.state_set.count()
-            if s == 1:
-                user = User.objects.get(pk=user_id)
-                user.state_set.create(name_state="here", user_id=user.id, reg_time=timezone.now())
-                qr.file.delete()
-                qr.delete()
-                return HttpResponseRedirect(reverse('user_here', args=(user_id, )))      
-            else:
-                form = QRForm()
-                error = 'Этот человек уже прошел'
-                qr.file.delete()
-                qr.delete()
-                return render(request, 'registr/scan.html', {'form': form, 'error': error})
+        try:
+            form = QRForm(request.POST, request.FILES)
+            if form.is_valid():
+                #тут должна быть выгрузка файла из формы 
+                qr = QR()
+                qr.file = form.cleaned_data["file"]
+                qr.file.save('qr.png', content=qr.file, save=True)
+                user_id = scanner('registr/files/qrs/qr.png')
+                who = User.objects.get(pk=user_id)
+                s = who.state_set.count()
+                if s == 1:
+                    user = User.objects.get(pk=user_id)
+                    user.state_set.create(name_state="here", user_id=user.id, reg_time=timezone.now())
+                    qr.file.delete()
+                    qr.delete()
+                    return HttpResponseRedirect(reverse('user_here', args=(user_id, )))      
+                else:
+                    form = QRForm()
+                    error = 'Этот человек уже прошел'
+                    qr.file.delete()
+                    qr.delete()
+                    return render(request, 'registr/scan.html', {'form': form, 'error': error})
+        except IndexError:   
+            error = 'Выберите QR-код'
+            qr.file.delete()
+            qr.delete()
+            return render(request, 'registr/scan.html', {'form': form, 'error': error})
     else:
-        form = QRForm()
+            form = QRForm()
+    
+
 
     return render(request, 'registr/scan.html', {'form': form, })
 
