f4f503df1fa349132e5947388f057d71c5e3964c
PolyaFilya <pollyfilippova51@gmail.com>
2020-06-10 20:11:17 +0500

    version 6.2

--- dsite/settings.py
+++ dsite/settings.py
@@ -109,16 +109,18 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'Indian/Maldives'
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False
 
 USE_TZ = True
 
+DATE_FORMAT = 'd E Y'
+TIME_FORMAT = 'H:i'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
